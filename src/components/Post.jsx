b7903f0443a980ecbcf6b1df99408cb6dfb69185
Loh Chun Mun <chunmun_loh@live.com>
2023-08-17 00:10:05 +0800

    output dynamic values

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,8 +1,13 @@
+const fruits = ['Apple', 'Orange'];
+
 const Post = () => {
+    const randomFruit = Math.random() > 0.5 ? fruits[0] : fruits[1];
+
     return (
         <div>
-            <h1>Hello world!</h1>
+            <h1>Hello World!</h1>
             <p>My first React.js component!</p>
+            <p>Fruit of the day: {randomFruit}</p>
         </div>
     );
 };
