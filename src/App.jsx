e4f790322eff656b456a135af6fd90057b1b6f89
Loh Chun Mun <chunmun_loh@live.com>
2023-08-17 00:20:38 +0800

    reusable props

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import Post from './components/Post';
 
 const App = () => {
-  return <Post />;
+  return (
+    <main>
+      <Post author="Chun Mun" body="React.js is awesome!" />
+      <Post author="CM" body="Check out the full course!" />
+    </main>
+  );
 };
 
 export default App;
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,13 +1,8 @@
-const fruits = ['Apple', 'Orange'];
-
-const Post = () => {
-    const randomFruit = Math.random() > 0.5 ? fruits[0] : fruits[1];
-
+const Post = (props) => {
     return (
         <div>
-            <h1>Hello World!</h1>
-            <p>My first React.js component!</p>
-            <p>Fruit of the day: {randomFruit}</p>
+            <p>{props.author}</p>
+            <p>{props.body}</p>
         </div>
     );
 };
