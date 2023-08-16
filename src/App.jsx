9a5ba18e0eb9efb7b791697960951e3f9d2d7f95
Loh Chun Mun <chunmun_loh@live.com>
2023-08-17 00:37:29 +0800

    PostsList component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
-import Post from './components/Post';
+import PostsList from "./components/PostsList";
 
 const App = () => {
   return (
     <main>
-      <Post author="Chun Mun" body="React.js is awesome!" />
-      <Post author="CM" body="Check out the full course!" />
+      <PostsList />
     </main>
   );
 };
