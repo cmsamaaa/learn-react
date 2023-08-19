6faafca76e22f701ae2788e92cdcfd3672c0cf52
Loh Chun Mun <chunmun_loh@live.com>
2023-08-20 01:42:26 +0800

    form submission handler

--- src/components/NewPost.jsx
+++ src/components/NewPost.jsx
@@ -1,15 +1,45 @@
+import { useState } from 'react';
+
 import classes from './NewPost.module.css';
 
-const NewPost = (props) => {
+const NewPost = ({ onCancel }) => {
+    // useState returns an array with 2 elements
+    // [0] - current value
+    // [1] - state updating function
+    const [enteredBody, setEnteredBody] = useState('');
+    const [enteredAuthor, setEnteredAuthor] = useState('');
+
+    const bodyChangeHandler = (event) => {
+        setEnteredBody(event.target.value);
+    };
+
+    const authorChangeHandler = (event) => {
+        setEnteredAuthor(event.target.value);
+    };
+
+    const submitHandler = (event) => {
+        event.preventDefault();
+        const postData = {
+            body: enteredBody,
+            author: enteredAuthor
+        };
+        console.log(postData);
+        onCancel();
+    };
+
     return (
-        <form className={classes.form}>
+        <form className={classes.form} onSubmit={submitHandler}>
             <p>
                 <label htmlFor="body">Text</label>
-                <textarea id="body" required rows={3} onChange={props.onBodyChange} />
+                <textarea id="body" required rows={3} onChange={bodyChangeHandler} />
             </p>
             <p>
                 <label htmlFor="name">Your name</label>
-                <input type="text" id="name" required onChange={props.onAuthorChange} />
+                <input type="text" id="name" required onChange={authorChangeHandler} />
+            </p>
+            <p className={classes.actions}>
+                <button type='button' onClick={onCancel}>Cancel</button>
+                <button>Submit</button>
             </p>
         </form>
     );
--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,25 +1,9 @@
-import { useState } from 'react';
-
 import Post from './Post';
 import NewPost from './NewPost';
 import Modal from './Modal';
 import classes from './PostsList.module.css';
 
 const PostsList = ({ isPosting, onStopPosting }) => {
-    // useState returns an array with 2 elements
-    // [0] - current value
-    // [1] - state updating function
-    const [enteredBody, setEnteredBody] = useState('');
-    const [enteredAuthor, setEnteredAuthor] = useState('');
-
-    const bodyChangeHandler = (event) => {
-        setEnteredBody(event.target.value);
-    };
-
-    const authorChangeHandler = (event) => {
-        setEnteredAuthor(event.target.value);
-    };
-
     // let modalContent;
 
     // if (modalIsVisible) {
@@ -37,14 +21,10 @@ const PostsList = ({ isPosting, onStopPosting }) => {
         <>
             {isPosting && (
                 <Modal onClose={onStopPosting}>
-                    <NewPost
-                        onBodyChange={bodyChangeHandler}
-                        onAuthorChange={authorChangeHandler}
-                    />
+                    <NewPost onCancel={onStopPosting} />
                 </Modal>
             )}
             <ul className={classes.posts}>
-                <Post author={enteredAuthor} body={enteredBody} />
                 <Post author="CM" body="Check out the full course!" />
             </ul>
         </>
